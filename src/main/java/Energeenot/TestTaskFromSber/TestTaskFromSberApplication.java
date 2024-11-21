31eff905068dcfe39506164ab2ccd64fc2cceb41
Energeenot <abramov.pandos@yandex.ru>
2024-11-21 18:30:25 +0300

    Initial commit

---
+++ src/main/java/Energeenot/TestTaskFromSber/TestTaskFromSberApplication.java
@@ -0,0 +1,13 @@
+package Energeenot.TestTaskFromSber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestTaskFromSberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestTaskFromSberApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Energeenot/TestTaskFromSber/TestTaskFromSberApplicationTests.java
@@ -0,0 +1,13 @@
+package Energeenot.TestTaskFromSber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestTaskFromSberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
